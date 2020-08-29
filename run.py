cddf00fe7570031fbf2ac42b1ee3ab268bd9348c
coinCrixus <coincrixus@protonmail.com>
2020-08-29 17:48:58 +0000

    get coingecko coin list

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
-import configparser
+import json
+import requests
+
 from sqlalchemy import create_engine
 from sqlalchemy import Column,String
 from sqlalchemy.ext.declarative import declarative_base  
@@ -9,23 +11,28 @@ load_dotenv()
 
 from models import CoinGeckoCoin
 
-db_string = os.getenv("DB_STRING")
 
+# setup db connection session
+db_string = os.getenv("DB_STRING")
 db = create_engine(db_string)  
 base = declarative_base()
-
 Session = sessionmaker(db)  
 session = Session()
 
+# get json coingecko coin list https://api.coingecko.com/api/v3/coins/list
+response = requests.get("https://api.coingecko.com/api/v3/coins/list")
+coins    = json.loads(response.text)
+
+for coin in coins:
+    print(coin)
+
+
 # Create 
-coin = CoinGeckoCoin('reserve0121','RSR','Reserve protocol')
-session.merge(coin)  
-session.commit()
+#coin = CoinGeckoCoin('reserve0121','RSR','Reserve protocol')
+#session.merge(coin)  
+#session.commit()
 
 # Read
-coins = session.query(CoinGeckoCoin)  
-for coin in coins:  
-    print(coin.symbol)
-
-
-
+#coins = session.query(CoinGeckoCoin)  
+#for coin in coins:  
+#    print(coin.symbol)
