f4520c1b0c6bc73b50ba4a493014e534098e70b2
coinCrixus <coincrixus@protonmail.com>
2020-08-29 19:28:54 +0000

    add deactivated columns

--- models.py
+++ models.py
@@ -19,11 +19,13 @@ class CoinGeckoCoin(Base):
     name   = Column(String)
     created_at = Column(DateTime(timezone=True), default=func.now())
     updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    deactivated = Column(Boolean, default=False)
 
-    def __init__(self, id, symbol, name):
+    def __init__(self, id, symbol, name, deactivated):
         self.id     = id
         self.symbol = symbol
         self.name   = name
+        self.deactivated = deactivated
 
 class CoinGeckoExchange(Base):
     __tablename__ = 'coingecko_exchange'
@@ -41,9 +43,10 @@ class CoinGeckoExchange(Base):
     trade_volume_24h_btc_normalized = Column(Numeric)
     created_at = Column(DateTime(timezone=True), default=func.now())
     updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    deactivated = Column(Boolean, default=False)
 
     def __init__(self, id, name, year_established, country, description, url, image, has_trading_incentive, trust_score, trust_score_rank,trade_volume_24h_btc
-                , trade_volume_24h_btc_normalized):
+                , trade_volume_24h_btc_normalized, deactivated):
         self.id     = id
         self.name   = name
         self.year_established = year_established
@@ -56,3 +59,4 @@ class CoinGeckoExchange(Base):
         self.trust_score_rank = trust_score_rank
         self.trade_volume_24h_btc = trade_volume_24h_btc
         self.trade_volume_24h_btc_normalized = trade_volume_24h_btc_normalized
+        self.deactivated = deactivated
--- run.py
+++ run.py
@@ -19,12 +19,15 @@ base = declarative_base()
 Session = sessionmaker(db)  
 session = Session()
 
+# deactivate current records
+CoinGeckoCoin.query.update({CoinGeckoCoin.deactivated: True})
+
 # get json coingecko coin list
 response = requests.get("https://api.coingecko.com/api/v3/coins/list")
 coins    = json.loads(response.text)
 
 for coin in coins:
-    coinObject = CoinGeckoCoin(coin['id'], coin['symbol'], coin['name'])
+    coinObject = CoinGeckoCoin(coin['id'], coin['symbol'], coin['name'], True)
     session.merge(coinObject)
 
 session.commit()
@@ -36,7 +39,7 @@ exchanges = json.loads(response.text)
 for exchange in exchanges:
     exchangeObject = CoinGeckoExchange(exchange['id'], exchange['name'], exchange['year_established'], exchange['country'], exchange['description'], exchange['url']
     , exchange['image'], exchange['has_trading_incentive'], exchange['trust_score'], exchange['trust_score_rank'], exchange['trade_volume_24h_btc'], exchange['trade_volume_24h_btc_normalized']
-    )
+    , True)
     session.merge(exchangeObject)
 
 session.commit()
