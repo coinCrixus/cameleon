2e3a2afe6130f530d82a1560f5df7eef09f5e96f
coinCrixus <coincrixus@protonmail.com>
2020-08-29 17:09:47 +0000

    add columns to tables

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean, Numeric
+from sqlalchemy import Column, Integer, String, Boolean, Numeric, DateTime
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -16,6 +17,8 @@ class CoinGeckoCoin(Base):
     id     = Column(String, primary_key=True) 
     symbol = Column(String)
     name   = Column(String)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
 
     def __init__(self, id, symbol, name):
         self.id     = id
@@ -36,4 +39,11 @@ class CoinGeckoExchange(Base):
     trust_score_rank = Column(Integer)
     trade_volume_24h_btc = Column(Numeric)
     trade_volume_24h_btc_normalized = Column(Numeric)
-    
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+
+    def __init__(self, id, name, year_established, country, description, url, image, has_trading_incentive, trust_score, trust_score_rank,trade_volume_24h_btc
+                , trade_volume_24h_btc_normalized):
+        self.id     = id
+        self.symbol = symbol
+        self.name   = name
