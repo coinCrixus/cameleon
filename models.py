12d561c83d8d58811bbe3360cc4a6000c85eec46
coinCrixus <coincrixus@protonmail.com>
2020-08-29 16:06:31 +0000

    add models for coingecko coins and exchanges

--- models.py
+++ models.py
@@ -1,12 +1,39 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, Numeric
 
 Base = declarative_base()
 
-class Student(Base):
-    __tablename__ = 'students'
-    name = Column(String)
-    seq = Column(Integer, primary_key=True)
+#class Student(Base):
+#    __tablename__ = 'students'
+#    name = Column(String)
+#    seq = Column(Integer, primary_key=True)
+#
+#    def __init__(self, name):
+#        self.name = name
 
-    def __init__(self, name):
-        self.name = name
+class CoinGeckoCoin(Base):
+    __tablename__ = 'coingecko_coin'
+    id     = Column(String, primary_key=True) 
+    symbol = Column(String)
+    name   = Column(String)
+
+    def __init__(self, id, symbol, name):
+        self.id     = id
+        self.symbol = symbol
+        self.name   = name
+
+class CoinGeckoExchange(Base):
+    __tablename__ = 'coingecko_exchange'
+    id      = Column(String, primary_key=True)
+    name    = Column(String)
+    year_established = Column(Integer)
+    country = Column(String)
+    description = Column(String)
+    url = Column(String)
+    image = Column(String)
+    has_trading_incentive = Column(Boolean)
+    trust_score = Column(Integer)
+    trust_score_rank = Column(Integer)
+    trade_volume_24h_btc = Column(Numeric)
+    trade_volume_24h_btc_normalized = Column(Numeric)
+    
