4cd17e130b198dfb7dd08a4a53360e46ed0e54d2
Ubuntu <ubuntu@ip-172-31-9-175.us-west-2.compute.internal>
2020-09-09 09:37:09 +0200

    Telegram bot message

--- sendMessageTelegramChannel.py
+++ sendMessageTelegramChannel.py
@@ -5,8 +5,10 @@ load_dotenv()
 
 msg = 'Hello btc world!'
 # use token generated in first step
-bot = telegram.Bot(token=os.getenv('TELEGRAM_API_TOKEN'))
-status = bot.send_message(chat_id="@olafbtcalerts", text=msg, parse_mode=telegram.ParseMode.HTML)
+telegram_token   = os.getenv('TELEGRAM_API_TOKEN')
+telegram_chat_id = os.getenv('TELEGRAM_API_CHAT_ID')
+bot = telegram.Bot(token=telegram_token)
+status = bot.send_message(chat_id=telegram_chat_id, text=msg, parse_mode=telegram.ParseMode.HTML)
 
 print(status)
 
